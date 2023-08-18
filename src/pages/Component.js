a03a54b4563d5b87e5c91d57957ca7d05ca7aae6
Jenna <jenna@Jennaui-MacBookPro.local>
2023-08-18 20:06:36 +0900

    initial commit

---
+++ src/pages/Component.js
@@ -0,0 +1,100 @@
+import "./Component.css";
+const Component = () => {
+  return (
+    <div className="div">
+      <div className="div1" />
+      <div className="div2">
+        <div className="child" />
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <div className="group-item" />
+          <div className="group-inner" />
+        </div>
+      </div>
+      <img className="arrow-back-ios" alt="" src="/arrow-back-ios.svg" />
+      <div className="div3">
+        <img className="item" alt="" src="/rectangle-68@2x.png" />
+        <div className="div4">닉네임 1</div>
+        <div className="wrapper">
+          <div className="div5">그래요 나 난 꿈이 있었죠</div>
+        </div>
+        <div className="container">
+          <div className="div5">버려지고 찢겨 남루하여도~</div>
+        </div>
+      </div>
+      <div className="div7">
+        <div className="rectangle-group">
+          <div className="rectangle-div" />
+          <div className="div8">2023년 8월 18일 금요일</div>
+        </div>
+      </div>
+      <div className="div9">
+        <img className="item" alt="" src="/rectangle-68@2x.png" />
+        <div className="div4">닉네임 2</div>
+        <div className="wrapper">
+          <div className="div5">내 가슴 깊숙히 보물과 같이</div>
+        </div>
+        <div className="frame-div">
+          <div className="div5">간직했던 꿈~</div>
+        </div>
+      </div>
+      <div className="div13">
+        <img className="item" alt="" src="/rectangle-68@2x.png" />
+        <div className="div4">닉네임 3</div>
+        <div className="wrapper">
+          <div className="div5">내 가슴 깊숙히 보물과 같이</div>
+        </div>
+        <div className="frame-div">
+          <div className="div5">간직했던 꿈~</div>
+        </div>
+      </div>
+      <img className="icon" alt="" src="/@2x.png" />
+      <div className="div17">
+        <div className="div5">혹 때론 누군가가~ 뜻 모를 비웃음</div>
+      </div>
+      <div className="div19">
+        <img className="icon1" alt="" src="/1.svg" />
+        <div className="div20" />
+        <img className="icon2" alt="" src="/2.svg" />
+        <div className="div21">
+          <span>오팔</span>
+          <span className="span">청춘</span>
+        </div>
+        <div className="div22">
+          <div className="child1" />
+          <div className="parent">
+            <b className="b">가</b>
+            <b className="b1">가</b>
+            <b className="b2">가</b>
+          </div>
+        </div>
+        <div className="div23" />
+      </div>
+      <img className="icon3" alt="" src="/3.svg" />
+      <div className="div24">
+        <div className="div25" />
+        <div className="group">
+          <div className="div26">
+            <img className="search-icon" alt="" src="/search.svg" />
+            <div className="div27">친구찾기</div>
+          </div>
+          <div className="div26">
+            <div className="div27">친구목록</div>
+            <img className="search-icon" alt="" src="/person.svg" />
+          </div>
+          <div className="div30">
+            <div className="div31">오늘의 인연</div>
+            <img className="auto-awesome-icon" alt="" src="/auto-awesome.svg" />
+          </div>
+          <div className="div32">
+            <div className="div33">1:1 대화방</div>
+            <img className="icon4" alt="" src="/11-.svg" />
+          </div>
+          <img className="icon5" alt="" src="/-@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Component;
